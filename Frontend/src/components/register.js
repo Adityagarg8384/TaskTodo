4ccba2746514de0e4eda4947d58963530cad5f3d
Adityagarg8384 <124500889+Adityagarg8384@users.noreply.github.com>
2024-03-11 22:59:29 +0530

    Update register.js

--- Frontend/src/components/register.js
+++ Frontend/src/components/register.js
@@ -13,7 +13,7 @@ const Register = () => {
 
     function Callbackend(){
         const t= JSON.stringify(data);
-        fetch("https://task-todo-two.vercel.app//register", {
+        fetch("https://task-todo-two.vercel.app/register", {
             method:'POST',
             body:t,
             headers: {
