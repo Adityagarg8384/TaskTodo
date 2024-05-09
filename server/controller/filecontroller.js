34c7077628dbc17dfd6eb502f69d449c45755d16
Adityagarg8384 <124500889+Adityagarg8384@users.noreply.github.com>
2024-05-10 02:44:09 +0530

    Update filecontroller.js

--- server/controller/filecontroller.js
+++ server/controller/filecontroller.js
@@ -8,7 +8,27 @@ exports.addtask = async (req, res) => {
         
         var { userid,id, task, setreminder, date } = req.body;
 
-        const response = await File.create({userid, id, task, setreminder, date });
+        let originalDate = new Date(date)
+        
+        originalDate.setMinutes(originalDate.getMinutes() - 30);
+
+        if(originalDate.getMinutes()<0){
+                    originalDate.setHours(originalDate.getHours() - 1);
+                    originalDate.setMinutes(originalMinutes.getMinutes() + 60);
+        };
+        
+        originalDate.setHours(originalDate.getHours() - 5);
+
+         if (originalDate.getHours() <0) {
+                    // Increment the date by 1 day
+                    originalDate.setDate(originalDate.getDate() - 1);
+                    originalDate.setHours(originalDate.getHours() + 24);
+        }
+        
+        let newDateString = originalDate.toISOString();
+
+        const response = await File.create({userid, id, task, setreminder, newDateString });
+        
         console.log(response);
 
         res.status(200).json({
